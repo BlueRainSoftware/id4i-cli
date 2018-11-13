7b59a4304d896db39eeddfc9e4570b90136f47aa
Wolfgang Werner <w.werner@vhmhv.de>
2018-11-13 13:44:34 +0100

    Add first subcommand example

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "id4i-cli",
+	Use:   "id4i",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
