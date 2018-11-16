b31cdfa55aaab4a9949694642a19f3a519b247c5
Wolfgang Werner <w.werner@vhmhv.de>
2018-11-16 18:28:56 +0100

    Add create GUID command

---
+++ cmd/create-guids.go
@@ -0,0 +1,63 @@
+// Copyright © 2018 Wolfgang Werner <mail@wolfgang-werner.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/BlueRainSoftware/id4i-cli/api_client/guids"
+	"github.com/BlueRainSoftware/id4i-cli/api_models"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	count  int32
+	length int32
+)
+
+var createGuidsCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create GUIDs",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		gr := api_models.CreateGUIDRequest{Count: &count, Length: &length, OrganizationID: &globCfgOrganization}
+
+		params := guids.NewCreateGUIDParams().WithCreateGUIDInfo(&gr)
+		resp, accepted, created, err := ID4i.Guids.CreateGUID(params, Bearer())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(accepted)
+		fmt.Println(created)
+		fmt.Printf("%#v\n", resp.Payload)
+	},
+}
+
+func init() {
+	guidsCmd.AddCommand(createGuidsCmd)
+
+	createGuidsCmd.Flags().Int32VarP(&count, "count", "c", 1, "Number of GUIDs to created between 1 and 1000")
+	createGuidsCmd.MarkFlagRequired("count")
+	createGuidsCmd.Flags().Int32VarP(&length, "length", "l", 7, "GUID length between 7 and 255")
+	createGuidsCmd.MarkFlagRequired("length")
+}
--- cmd/guids.go
+++ cmd/guids.go
@@ -23,61 +23,28 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"os"
 )
 
-var (
-	cfgShowConfig      bool
-	cfgShowBackendInfo bool
-)
-
-// infoCmd represents the info command
-var infoCmd = &cobra.Command{
-	Use:   "info",
-	Short: "Configuration and backend API information",
+// guidsCmd represents the guids command
+var guidsCmd = &cobra.Command{
+	Use:   "guids",
+	Short: "GUID operations",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("guids called")
 
-		if cfgShowConfig {
-			fmt.Printf("Config file: %s\n", viper.ConfigFileUsed())
-
-			for _, element := range viper.AllKeys() {
-				switch element {
-				case "secret":
-					fmt.Printf("%s\t%s\n", element, "*****")
-				default:
-					fmt.Printf("%s\t%s\n", element, viper.Get(element))
-				}
-			}
-		}
-
-		if cfgShowBackendInfo {
-			fmt.Printf("now call metadata api")
-
-			resp, accepted, err := ID4i.MetaInformation.ApplicationInfo(nil, Bearer())
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			fmt.Println(accepted)
-			fmt.Printf("%#v\n", resp.Payload)
-		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(infoCmd)
-
-	infoCmd.Flags().BoolVarP(&cfgShowConfig, "show-config", "", true, "Show configuration")
-	infoCmd.Flags().BoolVarP(&cfgShowBackendInfo, "show-backend", "", true, "Show backend information")
+	rootCmd.AddCommand(guidsCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// guidsCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// guidsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
